1d55d1376b4db47525fb0c14aab1cea7071ffbdb
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 07:24:37 +0700

    Add OrderOutbox🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/id/my/hendisantika/orderservice/entity/OrderOutbox.java
@@ -0,0 +1,60 @@
+package id.my.hendisantika.orderservice.entity;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-embedded-debezium-kafka-inbox-outbox-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/30/24
+ * Time: 07:24
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Entity
+@Table(name = "order_outbox")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderOutbox {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime createdDate;
+
+    private String aggregateType;
+
+    private String eventType;
+
+    @Column(length = 2000)
+    private String payload;
+
+    @Column(unique = true)
+    private String idempotentKey;
+
+    private OrderOutboxStatus status;
+
+    private Long orderId;
+
+}
