ff887b64caa16eb677e6b35c0079f9bfcd7e50c8
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 08:02:42 +0700

    Add kafkaTemplate in KafkaConfig🫸🌀✏️📗🐧🐳

--- stock-service/src/main/java/id/my/hendisantika/stockservice/config/KafkaConfig.java
+++ stock-service/src/main/java/id/my/hendisantika/stockservice/config/KafkaConfig.java
@@ -1,8 +1,10 @@
 package id.my.hendisantika.stockservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.KafkaTemplate;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +23,8 @@ public class KafkaConfig {
     @Value(value = "${kafka.bootstrapAddress}")
     private String bootstrapAddress;
 
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
