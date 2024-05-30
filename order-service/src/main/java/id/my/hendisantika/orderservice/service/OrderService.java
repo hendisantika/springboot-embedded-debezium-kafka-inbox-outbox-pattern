13581ee6ddeef355501daf0451bee1b843835fee
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 07:32:07 +0700

    Add toOutboxEntity in OrderService🫸🌀✏️📗🐧🐳

--- order-service/src/main/java/id/my/hendisantika/orderservice/service/OrderService.java
+++ order-service/src/main/java/id/my/hendisantika/orderservice/service/OrderService.java
@@ -1,12 +1,19 @@
 package id.my.hendisantika.orderservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.orderservice.dto.event.StockCreateEvent;
 import id.my.hendisantika.orderservice.dto.request.OrderRequestDto;
 import id.my.hendisantika.orderservice.entity.Order;
+import id.my.hendisantika.orderservice.entity.OrderOutbox;
+import id.my.hendisantika.orderservice.entity.enums.OrderOutboxStatus;
 import id.my.hendisantika.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +38,34 @@ public class OrderService {
         Order order = orderRepository.save(convertToOrder(orderRequestDto));
         outboxService.save(toOutboxEntity(order));
         return true;
+    }
+
+    private OrderOutbox toOutboxEntity(Order order) {
+        String payload = null;
+        String idempotentKey = RandomStringUtils.randomAlphanumeric(10);
+        try {
+            StockCreateEvent stockCreateEvent = StockCreateEvent.builder()
+                    .orderId(order.getId())
+                    .productId(order.getProductId())
+                    .idempotentKey(idempotentKey)
+                    .totalAmount(order.getTotalAmount())
+                    .build();
+
+            payload = objectMapper.writeValueAsString(stockCreateEvent);
+
+        } catch (JsonProcessingException ex) {
+            log.error("Object could not convert to String. Object: {}", order);
+            throw new RuntimeException(ex);
+        }
+
+        return OrderOutbox.builder()
+                .status(OrderOutboxStatus.CREATED)
+                .createdDate(LocalDateTime.now())
+                .idempotentKey(idempotentKey)
+                .payload(payload)
+                .aggregateType("Order")
+                .eventType("OrderCreated")
+                .orderId(order.getId())
+                .build();
     }
 }
