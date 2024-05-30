955580693a4413244730c30c38f21b0e271deac2
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 07:24:56 +0700

    Add OrderOutboxStatus🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/id/my/hendisantika/orderservice/entity/OrderOutboxStatus.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.orderservice.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-embedded-debezium-kafka-inbox-outbox-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/30/24
+ * Time: 07:24
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OrderOutboxStatus {
+    CREATED,
+    PROCESSED,
+    DONE
+}
