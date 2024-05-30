775ac207a4dde8cf4493db4e1df8d43af981694b
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 07:53:02 +0700

    Add createOrder in OrderController🫸🌀✏️📗🐧🐳

--- order-service/src/main/java/id/my/hendisantika/orderservice/controller/OrderController.java
+++ order-service/src/main/java/id/my/hendisantika/orderservice/controller/OrderController.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.orderservice.controller;
 
+import id.my.hendisantika.orderservice.dto.request.OrderRequestDto;
 import id.my.hendisantika.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +26,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<?> createOrder(@RequestBody OrderRequestDto orderRequestDto) {
+        return new ResponseEntity<>(
+                orderService.createOrder(orderRequestDto), HttpStatus.CREATED
+        );
+    }
 }
