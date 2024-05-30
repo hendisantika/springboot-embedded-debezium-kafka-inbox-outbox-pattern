c01d2d2f9817f97e9c7776dd8ee8980aec59b7ee
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 08:02:30 +0700

    Add KafkaConfig🫸🌀✏️📗🐧🐳

---
+++ stock-service/src/main/java/id/my/hendisantika/stockservice/config/KafkaConfig.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.stockservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-embedded-debezium-kafka-inbox-outbox-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/30/24
+ * Time: 08:02
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    @Value(value = "${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+}
