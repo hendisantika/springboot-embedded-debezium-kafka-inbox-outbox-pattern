2c2986e75f086ce12c55abbd96abd46bed963115
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 07:36:08 +0700

    Add DebeziumSourceEventListener🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/id/my/hendisantika/orderservice/listener/DebeziumSourceEventListener.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.orderservice.listener;
+
+import id.my.hendisantika.orderservice.service.OutboxService;
+import io.debezium.embedded.EmbeddedEngine;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-embedded-debezium-kafka-inbox-outbox-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/30/24
+ * Time: 07:35
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Component
+public class DebeziumSourceEventListener {
+
+    private final Executor executor = Executors.newSingleThreadExecutor();
+
+    private final EmbeddedEngine engine;
+    private final OutboxService outboxService;
+}
