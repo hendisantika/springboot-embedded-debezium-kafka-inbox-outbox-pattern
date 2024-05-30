7a56edf9203b167fe7750b4da45850af06104cda
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 07:41:47 +0700

    Add handleEvent in DebeziumSourceEventListener🫸🌀✏️📗🐧🐳

--- order-service/src/main/java/id/my/hendisantika/orderservice/listener/DebeziumSourceEventListener.java
+++ order-service/src/main/java/id/my/hendisantika/orderservice/listener/DebeziumSourceEventListener.java
@@ -1,15 +1,25 @@
 package id.my.hendisantika.orderservice.listener;
 
+import id.my.hendisantika.orderservice.entity.enums.Operation;
 import id.my.hendisantika.orderservice.service.OutboxService;
 import io.debezium.config.Configuration;
 import io.debezium.embedded.EmbeddedEngine;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+
+import static io.debezium.data.Envelope.FieldName.AFTER;
+import static io.debezium.data.Envelope.FieldName.OPERATION;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -48,6 +58,27 @@ public class DebeziumSourceEventListener {
     private void stop() {
         if (this.engine != null) {
             this.engine.stop();
+        }
+    }
+
+    private void handleEvent(SourceRecord sourceRecord) {
+        Struct sourceRecordValue = (Struct) sourceRecord.value();
+
+        if (sourceRecordValue != null) {
+            Operation operation = Operation.forCode((String) sourceRecordValue.get(OPERATION));
+
+            if (operation == Operation.CREATE) {
+
+                Struct struct = (Struct) sourceRecordValue.get(AFTER);
+                Map<String, Object> payload = struct.schema().fields().stream()
+                        .map(Field::name)
+                        .filter(fieldName -> struct.get(fieldName) != null)
+                        .map(fieldName -> Pair.of(fieldName, struct.get(fieldName)))
+                        .collect(toMap(Pair::getKey, Pair::getValue));
+
+                log.info("Data Changed: {} with Operation: {}", payload, operation.name());
+                outboxService.maintainReadModel(payload, operation);
+            }
         }
     }
 }
