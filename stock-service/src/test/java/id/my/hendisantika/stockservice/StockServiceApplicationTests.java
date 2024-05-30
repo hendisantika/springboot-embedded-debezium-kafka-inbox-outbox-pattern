94567a9ebed14c3f8fd8e308ddc289ff5c5bfcf5
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 07:57:58 +0700

    Add stock-service🫸🌀✏️📗🐧🐳

---
+++ stock-service/src/test/java/id/my/hendisantika/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
