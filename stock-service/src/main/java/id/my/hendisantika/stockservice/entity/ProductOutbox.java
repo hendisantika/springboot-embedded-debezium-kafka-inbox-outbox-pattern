712661f2bc40575ed6dba708073010a4f7e536fe
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 08:06:02 +0700

    Add ProductOutboxStatus Enum🫸🌀✏️📗🐧🐳

--- stock-service/src/main/java/id/my/hendisantika/stockservice/entity/ProductOutbox.java
+++ stock-service/src/main/java/id/my/hendisantika/stockservice/entity/ProductOutbox.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import id.my.hendisantika.stockservice.entity.enums.ProductOutboxStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
---
+++ stock-service/src/main/java/id/my/hendisantika/stockservice/entity/enums/ProductOutboxStatus.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.stockservice.entity.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-embedded-debezium-kafka-inbox-outbox-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/30/24
+ * Time: 08:05
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ProductOutboxStatus {
+    CREATED,
+    FAILED,
+    DONE
+}
